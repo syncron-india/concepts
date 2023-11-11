fd528879b31f7e2f5ee61887c9c70ab60866fe49
Ravindra1978 <ravindrab44@gmail.com>
2023-11-11 19:06:42 +0530

    updated read pgm

--- conver.sh
+++ conver.sh
@@ -2,5 +2,5 @@ echo "enter the value of person1"
 read person1
 echo "enter the value of person2"
 read person2
-echo "person1 name is $peros1"
+echo "person1 name is $person1"
 echo "person2 nname is $person2"
