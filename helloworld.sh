4da059386d9dc87e8aed8c59a259b0a61ad8670c
Ravindra1978 <ravindrab44@gmail.com>
2023-11-11 18:41:07 +0530

    conversation b/w two persons

---
+++ helloworld.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Hello world, I am learing DevOps"
+echo "Ramesh: Hi Suresh good morining"
+echo "Suresh: Hi Ramesh good morning"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I am good Ramesh"
